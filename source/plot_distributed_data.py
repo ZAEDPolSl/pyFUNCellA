1e9ec1abda3da4ae6a242dfb793c03f316eefedd
amrukwa <a.mrukwa00@gmail.com>
2021-11-15 22:33:57 +0000

    Fix the background

--- source/plot_distributed_data.py
+++ source/plot_distributed_data.py
@@ -8,20 +8,20 @@ from .model import get_predictions
 def plot_densities(data, model, comp_groups, score_name, geneset_name, save_dir="plots"):
     comp_colors ={"Non significant": "#0C5AA6", "Significant": "#FF9700"}
     label_meaning = ["Non significant", "Significant"]
-    plt.figure(figsize=(7,5))
+    f = plt.figure(figsize=(7,5))
 
     labels, binary_labels = get_predictions(model, comp_groups, data)
     comp_colors_density = "#808080"
     binary_labels = [label_meaning[label] for label in binary_labels ]        
         
-    sns.kdeplot(data, linewidth=2, color='k')
+    _ = sns.kdeplot(data, linewidth=2, color='k')
     
     if labels is not None:
-        sns.rugplot(data, clip_on=False, hue=labels)    
+        _ = sns.rugplot(data, clip_on=False, hue=labels)    
         
     plt.ylabel('Frequency')
     plt.xlabel(score_name)
-    plt.title(geneset_name)
+    plt.title(geneset_name, fontsize=25)
     
     for i in range(model.n_components):
         mu = model.means_[i, 0]
@@ -31,14 +31,15 @@ def plot_densities(data, model, comp_groups, score_name, geneset_name, save_dir=
         vals = stats.norm.pdf(x, mu, sigma)*model.weights_[i]
         plt.plot(x, vals, linestyle="dashed", color=comp_colors_density)
         
-    sns.rugplot(data, height=-.02, clip_on=False, hue=binary_labels,
+    _ = sns.rugplot(data, height=-.02, clip_on=False, hue=binary_labels,
                 palette = comp_colors)
+    f.set_facecolor('w')
     plt.savefig(save_dir+"/dens_"+geneset_name + "_" + score_name + ".png")
     
 def compare_with_categorical(score1, model, comp_groups, score_name1, geneset_name, score2, thr, score_name2, save_dir="plots"):
     comp_colors ={"Non significant": "#0C5AA6", "Significant": "#FF9700"}
     label_meaning = ["Non significant", "Significant"]
-    plt.figure(figsize=(7,10))
+    f = plt.figure(figsize=(7,10))
     
     
     labels, binary_labels = get_predictions(model, comp_groups, score1)
@@ -49,13 +50,10 @@ def compare_with_categorical(score1, model, comp_groups, score_name1, geneset_na
     ax1 = plt.subplot(211)
     ax1.set_xlabel(score_name1)
     ax1.set_ylabel("Frequency")
-    sns.kdeplot(score1, linewidth=2, color='k', ax=ax1)
+    _ = sns.kdeplot(score1, linewidth=2, color='k', ax=ax1)
     
     if labels is not None:
-        sns.rugplot(score1, clip_on=False, hue=labels, ax=ax1)    
-        
-    # plt.ylabel('Frequency')
-    # plt.xlabel(score_name1)
+        _ = sns.rugplot(score1, clip_on=False, hue=labels, ax=ax1)    
 
     for i in range(model.n_components):
         mu = model.means_[i, 0]
@@ -65,7 +63,7 @@ def compare_with_categorical(score1, model, comp_groups, score_name1, geneset_na
         vals = stats.norm.pdf(x, mu, sigma)*model.weights_[i]
         plt.plot(x, vals, linestyle="dashed", color=comp_colors_density)
         
-    sns.rugplot(score1, height=-.02, clip_on=False, hue=binary_labels,
+    _ = sns.rugplot(score1, height=-.02, clip_on=False, hue=binary_labels,
                 palette = comp_colors, ax=ax1)
     
     score_2 = (score2 > thr).astype(int)
@@ -75,9 +73,9 @@ def compare_with_categorical(score1, model, comp_groups, score_name1, geneset_na
     ax2.set_xlabel(score_name2)
     ax2.set_ylabel("Frequency")
     plt.axvline(x=thr, c="r", linestyle="dashed")
-    sns.kdeplot(score2, linewidth=2, color='k', ax=ax2)
-    sns.rugplot(score2, height=-.02, clip_on=False, hue=score_2,
+    _ = sns.kdeplot(score2, linewidth=2, color='k', ax=ax2)
+    _ = sns.rugplot(score2, height=-.02, clip_on=False, hue=score_2,
                 palette = comp_colors, ax=ax2)
-    
-    plt.suptitle(geneset_name)
+    f.set_facecolor('w')
+    plt.suptitle(geneset_name, fontsize=25)
     plt.savefig(save_dir+"/dens_"+geneset_name + "_" + score_name1 + "_"+score_name2 + ".png")
