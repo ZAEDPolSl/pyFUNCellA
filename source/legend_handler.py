1383339f4e20effe133152f367a2fec1f306b8b6
amrukwa <a.mrukwa00@gmail.com>
2022-01-04 20:28:12 +0000

    Fix legend appending

--- source/legend_handler.py
+++ source/legend_handler.py
@@ -5,7 +5,11 @@ def move_legend(ax, new_loc, append_to_labels=None, **kws):
     handles = old_legend.legendHandles
     labels = [t.get_text() for t in old_legend.get_texts()]
     labels, handles = zip(*sorted(zip(labels, handles), key=lambda t: t[0]))
+    append_to_labels = [x for x in append_to_labels if x != 0]
+    print(append_to_labels)
+    print(labels)
     if append_to_labels is not None:
         labels = [labels[i]+": "+str(append_to_labels[i]) for i in range(len(labels))]
+    print(labels)
     title = old_legend.get_title().get_text()
     ax.legend(handles, labels, loc=new_loc, title=title, **kws)
