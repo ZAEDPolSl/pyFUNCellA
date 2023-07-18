c28390fca1daac6b1fc0c82923b145a95469c191
amrukwa <a.mrukwa00@gmail.com>
2023-07-18 18:24:42 +0200

    improve ranking

--- test/test_metrics/rank_test.py
+++ test/test_metrics/rank_test.py
@@ -8,3 +8,17 @@ def test_rank_genes():
     res = rank.rank_genes(x)
     correct = np.array([[2, 2], [1, 1]])
     assert np.array_equal(correct, res)
+
+
+def test_rank_ties():
+    x = np.array([[2, 2, 1], [3, 5, 1], [1, 5, 1]])
+    res = rank.rank_genes(x)
+    correct = np.array([[2, 3, 2], [1, 1.5, 2], [3, 1.5, 2]])
+    assert np.array_equal(correct, res)
+
+
+def test_rank_ties_fisher():
+    x = np.array([[2, 2, 1], [3, 5, 1], [1, 5, 1]])
+    res = rank.rank_genes(x, ordinal=True)
+    correct = np.array([[2, 3, 1], [1, 1, 2], [3, 2, 3]])
+    assert np.array_equal(correct, res)
