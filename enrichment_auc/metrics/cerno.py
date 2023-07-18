3bd937b532d6e7cf8ed74e585e1e4611c9f30075
amrukwa <a.mrukwa00@gmail.com>
2023-07-18 21:49:51 +0200

    split cerno calculations

--- enrichment_auc/metrics/cerno.py
+++ enrichment_auc/metrics/cerno.py
@@ -1,11 +1,12 @@
 import numpy as np
-import pandas as pd
 from scipy import stats
 from statsmodels.stats.multitest import multipletests
 from tqdm import tqdm
 
+from enrichment_auc.metrics.rank import rank_genes
 
-def _cerno(
+
+def _fisher(
     geneset, data, genes, alpha=0.05, gs_name=""
 ):  # ordered gene names for each patient, geneset
     genes_in_ds = [gene in geneset for gene in genes]
@@ -16,29 +17,48 @@ def _cerno(
         print("Incorrect geneset format:", gs_name)
         return (
             np.zeros(data.shape[1]),
-            np.zeros(data.shape[1]),
             np.ones(data.shape[1]),
             np.ones(data.shape[1]),
         )
-    R = np.sum(in_gs, axis=0)
-    AUC = (N_gs * (N_tot - N_gs) + (N_gs + 1) * N_gs / 2 - R) / (N_gs * (N_tot - N_gs))
     cerno = np.sum(np.log(in_gs / N_tot), axis=0) * (-2)
     pvals = 1 - stats.chi2.cdf(cerno, 2 * N_gs)
     _, qvals, _, _ = multipletests(pvals, alpha=alpha, method="fdr_tsbh")
-    return cerno, AUC, pvals, qvals
+    return cerno, pvals, qvals
 
 
-def CERNO(
-    genesets, data, genes, alpha=0.05
-):  # ordered gene names for each patient, genesets
+def FISHER(genesets, data, genes, alpha=0.05):
+    data = rank_genes(data, ordinal=True)
     cerno = np.empty((len(genesets), data.shape[1]))
-    auc = np.empty((len(genesets), data.shape[1]))
     pval = np.empty((len(genesets), data.shape[1]))
     qval = np.empty((len(genesets), data.shape[1]))
     for i, (gs_name, geneset_genes) in tqdm(
         enumerate(genesets.items()), total=len(genesets)
     ):
-        cerno[i], auc[i], pval[i], qval[i] = _cerno(
-            geneset_genes, data, genes, alpha, gs_name
-        )
-    return cerno, auc, pval, qval
+        cerno[i], pval[i], qval[i] = _fisher(geneset_genes, data, genes, alpha, gs_name)
+    return cerno, pval, qval
+
+
+def _auc(
+    geneset, data, genes, gs_name=""
+):  # ordered gene names for each patient, geneset
+    genes_in_ds = [gene in geneset for gene in genes]
+    in_gs = data[genes_in_ds, :]
+    N_gs = in_gs.shape[0]  # number of genes in GS
+    N_tot = len(genes)  # total number of genes
+    if N_tot <= N_gs or N_gs == 0:
+        print("Incorrect geneset format:", gs_name)
+        return np.zeros(data.shape[1])
+    R = np.sum(in_gs, axis=0)
+    auc = N_gs * (N_tot - N_gs) + (N_gs + 1) * N_gs / 2 - R
+    auc /= N_gs * (N_tot - N_gs)
+    return auc
+
+
+def AUC(genesets, data, genes):
+    data = rank_genes(data)
+    auc = np.empty((len(genesets), data.shape[1]))
+    for i, (gs_name, geneset_genes) in tqdm(
+        enumerate(genesets.items()), total=len(genesets)
+    ):
+        auc[i] = _auc(geneset_genes, data, genes, gs_name)
+    return auc
--- test/test_metrics/cerno_test.py
+++ test/test_metrics/cerno_test.py
@@ -4,7 +4,7 @@ from statsmodels.stats.multitest import multipletests
 import enrichment_auc.metrics.cerno as cerno
 
 
-def test_cerno_auc():
+def test_auc():
     genes = ["a", "b", "c", "d", "e", "g"]
     genesets = {"geneset": ["a", "b", "e", "f"]}
     data = np.array(
@@ -17,12 +17,30 @@ def test_cerno_auc():
             [3, 6, 4, 1, 6, 1],
         ]
     )
-    cerno_, auc, _, pval = cerno._cerno(genesets["geneset"], data, genes)
+    auc = cerno._auc(genesets["geneset"], data, genes)
     auc_expected = np.array([2 / 3, 1 / 3, 2 / 9, 3 / 9, 7 / 9, 2 / 9])
     assert np.array_equal(auc_expected, auc)
 
 
-def test_cerno():
+def test_auc_ties():
+    genes = ["a", "b", "c", "d", "e", "g"]
+    genesets = {"geneset": ["a", "b", "e", "f"]}
+    data = np.array(
+        [
+            [2.0, 3.5, 5.0, 4.0, 1.0, 6.0],
+            [1.0, 5.0, 3.0, 5.0, 2.0, 5.0],
+            [4.0, 2.0, 3.0, 2.0, 3.0, 4.0],
+            [5.0, 1.0, 1.0, 6.0, 4.0, 3.0],
+            [6.0, 3.5, 6.0, 3.0, 5.0, 2.0],
+            [3.0, 6.0, 3.0, 1.0, 6.0, 1.0],
+        ]
+    )
+    auc = cerno._auc(genesets["geneset"], data, genes)
+    auc_expected = np.array([2.0 / 3, 1.0 / 3, 1.0 / 9, 1.0 / 3, 7.0 / 9, 2.0 / 9])
+    assert np.array_equal(auc_expected, auc)
+
+
+def test_fisher():
     genes = ["a", "b", "c", "d", "e", "g"]
     genesets = {"geneset": ["a", "b", "e", "f"]}
     data = np.array(
@@ -35,7 +53,7 @@ def test_cerno():
             [3, 6, 4, 1, 6, 1],
         ]
     )
-    cerno_, auc, _, pval = cerno._cerno(genesets["geneset"], data, genes)
+    cerno_, _, _ = cerno._fisher(genesets["geneset"], data, genes)
     cerno_expected = np.array([5.7807, 2.5619, 2.5619, 2.5619, 6.1454, 2.5619])
     assert np.isclose(cerno_expected, cerno_, atol=10e-4).all()
 
@@ -54,7 +72,7 @@ def test_pval():
             [3, 6, 4, 1, 6, 1],
         ]
     )
-    cerno_, auc, pval, qval = cerno._cerno(genesets["geneset"], data, genes)
+    _, pval, qval = cerno._fisher(genesets["geneset"], data, genes)
     pval_expected = [
         0.44819423,
         0.86148045,
@@ -68,7 +86,7 @@ def test_pval():
     assert np.isclose(pval, pval_expected, atol=10e-4).all()
 
 
-def test_cerno_format():
+def test_fisher_format():
     genes = ["a", "b", "c", "d", "e", "g"]
     genesets = {"geneset": ["a", "b", "c", "d", "e", "g"]}
     data = np.array(
@@ -81,9 +99,26 @@ def test_cerno_format():
             [3, 6, 4, 1, 6, 1],
         ]
     )
-    cerno_, auc, pval, qval = cerno._cerno(genesets["geneset"], data, genes)
-    auc_expected = np.array([0, 0, 0, 0, 0, 0])
-    assert np.array_equal(auc_expected, auc)
-    assert np.array_equal(auc_expected, cerno_)
+    cerno_, pval, qval = cerno._fisher(genesets["geneset"], data, genes)
+    cerno_expected = np.array([0, 0, 0, 0, 0, 0])
+    assert np.array_equal(cerno_expected, cerno_)
     assert np.array_equal(np.array([1, 1, 1, 1, 1, 1]), pval)
     assert np.array_equal(np.array([1, 1, 1, 1, 1, 1]), qval)
+
+
+def test_auc_format():
+    genes = ["a", "b", "c", "d", "e", "g"]
+    genesets = {"geneset": ["a", "b", "c", "d", "e", "g"]}
+    data = np.array(
+        [
+            [2, 4, 5, 4, 1, 6],
+            [1, 5, 2, 5, 2, 5],
+            [4, 2, 3, 2, 3, 4],
+            [5, 1, 1, 6, 4, 3],
+            [6, 3, 6, 3, 5, 2],
+            [3, 6, 4, 1, 6, 1],
+        ]
+    )
+    auc = cerno._auc(genesets["geneset"], data, genes)
+    auc_expected = np.array([0, 0, 0, 0, 0, 0])
+    assert np.array_equal(auc_expected, auc)
