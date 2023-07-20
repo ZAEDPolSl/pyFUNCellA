871ae82baffd996487ee6ae1a3663514c584ebcb
amrukwa <a.mrukwa00@gmail.com>
2023-07-20 10:33:34 +0000

    add progress bar display

--- enrichment_auc/metrics/gsea.R
+++ enrichment_auc/metrics/gsea.R
@@ -12,10 +12,7 @@ library("GSVA")
 args <- commandArgs(trailingOnly=TRUE)
 method <- args[1]
 outpath <- args[2]
-print("x")
 pathways <- fromJSON(file=paste(outpath, "tmp/genesets_genes.json", sep=""))
 gene_expr <- read.csv(paste(outpath, "tmp/data.csv",  sep=""), row.names = 1, header= TRUE)
-print("y")
 res <- gsva(as.matrix(gene_expr), pathways, method = method, kcdf = "Gaussian", mx.diff = F)
-print("z")
 write.csv(as.data.frame(res), paste(outpath, "tmp/res.csv", sep=""))
--- enrichment_auc/metrics/gsea.py
+++ enrichment_auc/metrics/gsea.py
@@ -2,24 +2,24 @@ import json
 import os
 import shutil
 import sys
-from subprocess import PIPE, Popen
+import subprocess
 
 import pandas as pd
 
 
 def _run_analysis(genesets, data, genes, method):
-    os.makedirs("tmp")
+    if not os.path.isdir("tmp"):
+        os.makedirs("tmp")
     df = pd.DataFrame(data, index=genes)
     df.to_csv("tmp/data.csv")
+    del df
     with open("tmp/genesets_genes.json", "w") as fp:
         json.dump(genesets, fp)
     rscript = "enrichment_auc/metrics/gsea.R"
     outpath = os.getcwd() + "/"
     cmd = ["Rscript", rscript] + [x for x in [method, outpath]]
-    destination = PIPE
-    destination = sys.stderr
-    sp = Popen(cmd, stdout=PIPE, stderr=destination)
-    sp.communicate()
+    cmd = " ".join(cmd)
+    subprocess.call(cmd, shell=True)
     res = pd.read_csv("tmp/res.csv", index_col=0, header=0)
     shutil.rmtree("tmp")
     return res.to_numpy()
