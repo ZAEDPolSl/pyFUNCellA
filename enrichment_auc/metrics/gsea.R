f6f60268e505956c4bc51e987eb1ef628bfd50d9
amrukwa <a.mrukwa00@gmail.com>
2023-07-20 19:09:04 +0000

    fix gsea for bigger datasets

--- enrichment_auc/metrics/gsea.R
+++ enrichment_auc/metrics/gsea.R
@@ -14,5 +14,13 @@ method <- args[1]
 outpath <- args[2]
 pathways <- fromJSON(file=paste(outpath, "tmp/genesets_genes.json", sep=""))
 gene_expr <- read.csv(paste(outpath, "tmp/data.csv",  sep=""), row.names = 1, header= TRUE)
-res <- gsva(as.matrix(gene_expr), pathways, method = method, kcdf = "Gaussian", mx.diff = F)
+
+tryCatch(
+{
+  res <- gsva(as.matrix(gene_expr), pathways, method = method, kcdf = "Gaussian", mx.diff = F) 
+},
+error=function(e) {
+    print(e)
+    res <- gene_expr
+})
 write.csv(as.data.frame(res), paste(outpath, "tmp/res.csv", sep=""))
