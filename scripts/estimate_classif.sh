6d4b71296925849ef1cc54107b7c2271ac6ce7c7
amrukwa <a.mrukwa00@gmail.com>
2023-07-24 22:38:48 +0200

    add classification scripts

---
+++ scripts/estimate_classif.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+norm='seurat'
+declare -a data_types=('PBMC', 'Liver', 'COVID', 'BM')
+declare -a cluster_types=('gmm', 'kmeans')
+
+
+data_folder="data/"
+res_folder="results/"
+
+for data_type in ${data_types[@]}; do
+    for cluster_type in ${cluster_types[@]}; do
+        python estimate_classif.py $data_type $norm $cluster_type $data_folder $res_folder
+        done
+done
