3395d638b72d07970895053efc8877d84d8fbcd5
amrukwa <a.mrukwa00@gmail.com>
2023-10-06 13:38:10 +0200

    fix SW param

--- gamred_native/calculate_SW.c
+++ gamred_native/calculate_SW.c
@@ -11,7 +11,6 @@
 #include "rt_nonfinite.h"
 #include <math.h>
 
-
 /* Function Definitions */
 /*
  * function [ SW ] = calculate_SW( x, KS )
@@ -26,44 +25,65 @@ double calculate_SW(const emxArray_real_T *x, double KS)
   int n;
   boolean_T exitg1;
   /*      SW = iqr(x)/(10*KS); */
-  /* 'calculate_SW:3' SW = ((max(x)-min(x))/(5*KS))^2; */
+  /* 'calculate_SW:3' SW = ((max(x)-min(x))/(4*KS))^2; */
   n = x->size[0];
-  if (x->size[0] <= 2) {
-    if (x->size[0] == 0) {
+  if (x->size[0] <= 2)
+  {
+    if (x->size[0] == 0)
+    {
       ex = rtNaN;
-    } else if (x->size[0] == 1) {
-      ex = x->data[0];
-    } else if ((x->data[0] < x->data[1]) || (rtIsNaN(x->data[0]) && (!rtIsNaN
-                 (x->data[1])))) {
-      ex = x->data[1];
-    } else {
+    }
+    else if (x->size[0] == 1)
+    {
       ex = x->data[0];
     }
-  } else {
-    if (!rtIsNaN(x->data[0])) {
+    else if ((x->data[0] < x->data[1]) || (rtIsNaN(x->data[0]) && (!rtIsNaN(x->data[1]))))
+    {
+      ex = x->data[1];
+    }
+    else
+    {
+      ex = x->data[0];
+    }
+  }
+  else
+  {
+    if (!rtIsNaN(x->data[0]))
+    {
       idx = 1;
-    } else {
+    }
+    else
+    {
       idx = 0;
       k = 2;
       exitg1 = false;
-      while ((!exitg1) && (k <= x->size[0])) {
-        if (!rtIsNaN(x->data[k - 1])) {
+      while ((!exitg1) && (k <= x->size[0]))
+      {
+        if (!rtIsNaN(x->data[k - 1]))
+        {
           idx = k;
           exitg1 = true;
-        } else {
+        }
+        else
+        {
           k++;
         }
       }
     }
 
-    if (idx == 0) {
+    if (idx == 0)
+    {
       ex = x->data[0];
-    } else {
+    }
+    else
+    {
       ex = x->data[idx - 1];
       idx++;
-      for (k = idx; k <= n; k++) {
+      for (k = idx; k <= n; k++)
+      {
         d = x->data[k - 1];
-        if (ex < d) {
+        if (ex < d)
+        {
           ex = d;
         }
       }
@@ -71,49 +91,70 @@ double calculate_SW(const emxArray_real_T *x, double KS)
   }
 
   n = x->size[0];
-  if (x->size[0] <= 2) {
-    if (x->size[0] == 0) {
+  if (x->size[0] <= 2)
+  {
+    if (x->size[0] == 0)
+    {
       b_ex = rtNaN;
-    } else if (x->size[0] == 1) {
-      b_ex = x->data[0];
-    } else if ((x->data[0] > x->data[1]) || (rtIsNaN(x->data[0]) && (!rtIsNaN
-                 (x->data[1])))) {
-      b_ex = x->data[1];
-    } else {
+    }
+    else if (x->size[0] == 1)
+    {
       b_ex = x->data[0];
     }
-  } else {
-    if (!rtIsNaN(x->data[0])) {
+    else if ((x->data[0] > x->data[1]) || (rtIsNaN(x->data[0]) && (!rtIsNaN(x->data[1]))))
+    {
+      b_ex = x->data[1];
+    }
+    else
+    {
+      b_ex = x->data[0];
+    }
+  }
+  else
+  {
+    if (!rtIsNaN(x->data[0]))
+    {
       idx = 1;
-    } else {
+    }
+    else
+    {
       idx = 0;
       k = 2;
       exitg1 = false;
-      while ((!exitg1) && (k <= x->size[0])) {
-        if (!rtIsNaN(x->data[k - 1])) {
+      while ((!exitg1) && (k <= x->size[0]))
+      {
+        if (!rtIsNaN(x->data[k - 1]))
+        {
           idx = k;
           exitg1 = true;
-        } else {
+        }
+        else
+        {
           k++;
         }
       }
     }
 
-    if (idx == 0) {
+    if (idx == 0)
+    {
       b_ex = x->data[0];
-    } else {
+    }
+    else
+    {
       b_ex = x->data[idx - 1];
       idx++;
-      for (k = idx; k <= n; k++) {
+      for (k = idx; k <= n; k++)
+      {
         d = x->data[k - 1];
-        if (b_ex > d) {
+        if (b_ex > d)
+        {
           b_ex = d;
         }
       }
     }
   }
 
-  ex = (ex - b_ex) / (5.0 * KS);
+  ex = (ex - b_ex) / (4.0 * KS);
   return ex * ex;
 }
 
