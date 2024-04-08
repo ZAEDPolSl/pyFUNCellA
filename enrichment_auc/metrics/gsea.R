625da18adbd8d92f87eec00bfe496dce3479087c
amrukwa <a.mrukwa00@gmail.com>
2024-04-08 15:07:10 +0000

    Add renv

--- enrichment_auc/metrics/gsea.R
+++ enrichment_auc/metrics/gsea.R
@@ -1,5 +1,6 @@
 if (!require("BiocManager", quietly = TRUE))
     install.packages("BiocManager", repos = "http://cran.us.r-project.org")
+BiocManager::install(version = "3.18")
 if (!require("GSVA", quietly = TRUE))
     BiocManager::install("GSVA")
 if (!require("rjson", quietly = TRUE))
@@ -13,14 +14,13 @@ args <- commandArgs(trailingOnly=TRUE)
 method <- args[1]
 outpath <- args[2]
 pathways <- fromJSON(file=paste(outpath, "tmp/genesets_genes.json", sep=""))
-gene_expr <- read.csv(paste(outpath, "tmp/data.csv",  sep=""), row.names = 1, header= TRUE)
-
+gene_expr <- read.csv(paste(outpath, "tmp/data.csv",  sep=""), row.names=1, header=TRUE, check.names=FALSE)
+res <- gene_expr
 tryCatch(
 {
   res <- gsva(as.matrix(gene_expr), pathways, method = method, kcdf = "Gaussian", mx.diff = F) 
 },
 error=function(e) {
-    print(e)
-    res <- gene_expr
+    conditionMessage(e)
 })
 write.csv(as.data.frame(res), paste(outpath, "tmp/res.csv", sep=""))
