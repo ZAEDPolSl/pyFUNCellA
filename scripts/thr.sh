a4662bc82906e9630a6c23fcfc90158c655ba2b7
amrukwa <a.mrukwa00@gmail.com>
2024-04-10 16:57:47 +0000

    fix the loop

--- scripts/thr.sh
+++ scripts/thr.sh
@@ -1,15 +1,14 @@
 #! /bin/bash
 # poetry install
 # poetry build
-# declare -a data_types=('seurat' 'row')
-declare -a norms=('seurat')
-data_type='PBMC' # Liver BM COVID
+norm='seurat'
+declare -a data_types=('PBMC' 'Liver' 'BM' 'COVID')
 
-data_folder="/mnt/pmanas/Ania/scrna-seq/data/${data_type}/"
-res_folder="/mnt/pmanas/Ania/scrna-seq/results/${data_type}/"
-plot_folder="/mnt/pmanas/Ania/scrna-seq/plots/${data_type}"
 # Iterate the string array using for loop
-for norm in ${norms[@]}; do
+for data_type in ${data_types[@]}; do
+    data_folder="/mnt/pmanas/Ania/scrna-seq/data/${data_type}/"
+    res_folder="/mnt/pmanas/Ania/scrna-seq/results/${data_type}/"
+    plot_folder="/mnt/pmanas/Ania/scrna-seq/plots/${data_type}"
      python scripts/escript.py $norm $res_folder $plot_folder $data_folder
     #  zip results
     # cd $"${res_folder}${norm}"
