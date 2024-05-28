c2b5f7e782d869d91a615f2fcd0321d6f09a8612
amrukwa <a.mrukwa00@gmail.com>
2024-05-28 12:33:19 +0000

    add additional visualization

---
+++ scripts/visualize_aucell.sh
@@ -0,0 +1,23 @@
+#! /bin/bash
+# poetry install
+# poetry build
+norm='seurat'
+declare -a data_types=('Liver') # 'Liver'
+
+# Iterate the string array using for loop
+for data_type in ${data_types[@]}; do
+    data_folder="/mnt/pmanas/Ania/scrna-seq/data/${data_type}/"
+    res_folder="/mnt/pmanas/Ania/scrna-seq/results/${data_type}/"
+    plot_folder="/mnt/pmanas/Ania/scrna-seq/plots/${data_type}"
+     python scripts/visualize_aucell.py $norm $res_folder $plot_folder $data_folder
+    #  zip results
+    # cd $"${res_folder}${norm}"
+    # zip "${norm}.zip" *.json
+    # zip -r "${norm}.zip" kmeans_thr/ gmm_thr/
+    # zip -r "${norm}.zip" times_thrs.csv
+    # cd $"../../../${plot_folder}/${norm}"
+    # zip -r "${norm}.zip" kmeans/ top1/
+    # find . -type f -iname \*.png -delete
+    # find . -type f -iname \*.html -delete
+    # cd ../../../
+done
