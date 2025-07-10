4cb57d0cfb1b9ce2f833096c03e06596567803ab
amrukwa <a.mrukwa00@gmail.com>
2025-07-10 16:53:13 +0200

    remove additional scripts

--- scripts/calculate_scores.sh
+++
@@ -1,9 +0,0 @@
-#! /bin/bash
-# declare -a data_types=('seurat' 'row')
-declare -a data_types=('seurat')
-inpath="/mnt/pmanas/Ania/scrna-seq/data/BreastCancer"
-outpath="/mnt/pmanas/Ania/scrna-seq/results/BreastCancer"
-# Iterate the string array using for loop
-for data_type in ${data_types[@]}; do
-     python scripts/calculate_scores.py $inpath $outpath $data_type
-done
