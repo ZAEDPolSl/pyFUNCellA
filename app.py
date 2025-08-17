961d43a70f72a21de825bd266d05deb20559633d
amrukwa <a.mrukwa00@gmail.com>
2025-08-17 15:15:24 +0200

    update the UI

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 import streamlit as st
 
+
 st.set_page_config(page_title="FUNCellA", page_icon="🧬")
 
 from app.pas_tab import pas_tab
@@ -17,5 +18,49 @@ with tabs[0]:
     upload_tab()
 with tabs[1]:
     pas_tab()
+
 with tabs[2]:
     threshold_tab()
+
+
+footer = """
+<style>
+.footer {
+    position: fixed;
+    left: 0;
+    bottom: 0;
+    width: 100%;
+    background-color: #154c8e;
+    color: white;
+    padding: 10px;
+    z-index: 100;
+    display: flex;
+    align-items: center;
+}
+.footer-logos {
+    display: flex;
+    align-items: center;
+}
+.footer img {
+    margin: 0px 10px;
+    vertical-align: middle;
+    height: 40px;
+    transition: transform 0.2s;
+}
+.footer img:hover {
+    transform: scale(1.2);
+}
+.footer-text {
+    margin-left: auto;
+    margin-right: 20px;
+}
+</style>
+<div class="footer">
+    <div class="footer-logos">
+        <img src="https://raw.githubusercontent.com/ZAEDPolSl/enrichment-auc/main/app/static/politechnika_sl_logo_poziom_inwersja_en_rgb.png" alt="Politechnika Śląska Logo">
+        <img src="https://raw.githubusercontent.com/ZAEDPolSl/enrichment-auc/main/app/static/EN-logo-poziome-rgb.png" alt="EN Logo">
+    </div>
+</div>
+"""
+
+st.markdown(footer, unsafe_allow_html=True)
