bbc6b528651cc29b82b196124c04ddce7956b2df
amrukwa <a.mrukwa00@gmail.com>
2025-08-20 08:52:21 +0200

    add metadata failsafe

--- pyfuncella/__init__.py
+++ pyfuncella/__init__.py
@@ -16,19 +16,42 @@ def load_pathways(kind: str):
     -------
     pd.DataFrame | dict
     """
+    import tempfile
+    import urllib.request
+    import os
+
     if kind == "info":
         filename = "pathway_info.csv"
-        with importlib.resources.files(__package__ + ".data").joinpath(filename).open(
-            "r", encoding="utf-8"
-        ) as f:
-            return pd.read_csv(f)
+        github_url = "https://raw.githubusercontent.com/ZAEDPolSl/pyFUNCellA/main/pyfuncella/data/pathway_info.csv"
+        try:
+            with importlib.resources.files("pyfuncella.data").joinpath(filename).open(
+                "r", encoding="utf-8"
+            ) as f:
+                return pd.read_csv(f)
+        except FileNotFoundError:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".csv") as tmp:
+                urllib.request.urlretrieve(github_url, tmp.name)
+                tmp.close()
+                df = pd.read_csv(tmp.name)
+            os.unlink(tmp.name)
+            return df
 
     elif kind == "data":
         filename = "pathways.json"
-        with importlib.resources.files(__package__ + ".data").joinpath(filename).open(
-            "r", encoding="utf-8"
-        ) as f:
-            return json.load(f)
+        github_url = "https://raw.githubusercontent.com/ZAEDPolSl/pyFUNCellA/main/pyfuncella/data/pathways.json"
+        try:
+            with importlib.resources.files("pyfuncella.data").joinpath(filename).open(
+                "r", encoding="utf-8"
+            ) as f:
+                return json.load(f)
+        except FileNotFoundError:
+            with tempfile.NamedTemporaryFile(delete=False, suffix=".json") as tmp:
+                urllib.request.urlretrieve(github_url, tmp.name)
+                tmp.close()
+                with open(tmp.name, "r", encoding="utf-8") as f:
+                    data = json.load(f)
+            os.unlink(tmp.name)
+            return data
 
     else:
         raise ValueError("kind must be either 'info' or 'data'")
